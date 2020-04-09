e350ca4f587b5fb3aa038de5e625a7f7fbe81c61
erosbalint3 <erosbalint3@gmail.com>
2020-04-09 13:04:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Balint");
+let printNumbersTill = (n) => {
+    for(let counter = 1; counter <= n; counter++){
+        console.log(counter);
+    }
+}
+let getGreetingTo = (name) => {
+    return "Hello " + name;
+}
+let myLista = [0,3,6,7,9];
+myLista.forEach((number) => {
+    console.log(number);
+});
